f247d2b90e0a0971afda4f64c746106c4c039b17
fentercj <fentercj@mail.uc.edu>
2021-03-08 18:58:54 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*My partner for this lab is Jonathan Ashbrook*/
+
+class Main {
+  public static void main(String[] args) {
+    
+    Tree tree1 = new Tree();
+    tree1.print();
+
+    Tree tree2 = new Tree("maple", "broadleaf", true, "red");
+    tree2.print();
+
+    tree1.setName("spruce");
+    tree1.setType("conifer");
+    tree1.print();
+  }
+}
